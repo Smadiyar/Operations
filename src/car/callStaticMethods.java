901fb257eb7b0ec27c82fe0ea6e990c61c57f68d
Tanpan <tanpan@SMD-MacBook-Air-2.local>
2019-05-30 19:38:19 -0500

    changed one

--- src/car/callStaticMethods.java
+++ src/car/callStaticMethods.java
@@ -3,13 +3,16 @@ package car;
 public class callStaticMethods {
 	public static void main(String[] args) {
 		
-		staticMethod.name="John";
-		staticMethod.age=35;
-		staticMethod.dateOfBirth="01/25/1984";
-		staticMethod.getPersonInfo();
+//		staticMethod.name="John";
+//		staticMethod.age=35;
+//		staticMethod.dateOfBirth="01/25/1984";
+//		staticMethod.getPersonInfo();
 		
 		
-		
+		staticMethod.name="Tammy";
+		staticMethod.age=25;
+		staticMethod.dateOfBirth="03/25/1985";
+		staticMethod.getPersonInfo();	
 		
 		
 		
